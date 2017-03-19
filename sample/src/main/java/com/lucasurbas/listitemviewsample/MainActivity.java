0f2a555790c2401ea7562f90d3d88e14d28bd7d3
Lucas Urbas <lucas.urbas@gmail.com>
2017-03-19 12:13:08 +0000

    Update sample

--- sample/src/main/java/com/lucasurbas/listitemviewsample/MainActivity.java
+++ sample/src/main/java/com/lucasurbas/listitemviewsample/MainActivity.java
@@ -11,14 +11,8 @@ import com.lucasurbas.listitemview.ListItemView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.list_item_view_1)
-    ListItemView listItemView1;
-
-    @BindView(R.id.list_item_view_2)
-    ListItemView listItemView2;
-
-    @BindView(R.id.list_item_view_3)
-    ListItemView listItemView3;
+    @BindView(R.id.list_item_view)
+    ListItemView listItemView;
 
     @BindView(R.id.attr_title)
     ListItemView attributeTitleView;
@@ -100,34 +94,26 @@ public class MainActivity extends AppCompatActivity {
     private void onAttrTitleClicked() {
         attrTitle = !attrTitle;
         attributeTitleView.inflateMenu(attrTitle ? R.menu.uncheck_menu : R.menu.check_menu);
-        listItemView1.setTitle(attrTitle ? getString(R.string.title) : null);
-        listItemView2.setTitle(attrTitle ? getString(R.string.title) : null);
-        listItemView3.setTitle(attrTitle ? getString(R.string.title) : null);
+        listItemView.setTitle(attrTitle ? getString(R.string.title) : null);
     }
 
     private void onAttrSubtitleClicked() {
         attrSubtitle = !attrSubtitle;
         attributeSubtitleView.inflateMenu(attrSubtitle ? R.menu.uncheck_menu : R.menu.check_menu);
-        listItemView1.setSubtitle(attrSubtitle ? getString(R.string.subtitle_long) : null);
-        listItemView2.setSubtitle(attrSubtitle ? getString(R.string.subtitle_long) : null);
-        listItemView3.setSubtitle(attrSubtitle ? getString(R.string.subtitle_long) : null);
+        listItemView.setSubtitle(attrSubtitle ? getString(R.string.subtitle_long) : null);
     }
 
     private void onAttrMultilineClicked() {
         multiline = !multiline;
         attributeMultilineView.inflateMenu(multiline ? R.menu.uncheck_menu : R.menu.check_menu);
-        listItemView1.setMultiline(multiline);
-        listItemView2.setMultiline(multiline);
-        listItemView3.setMultiline(multiline);
+        listItemView.setMultiline(multiline);
     }
 
     private void onAttrForceKeylineClicked() {
         forceKeyline = !forceKeyline;
         attributeForceKeylineView.inflateMenu(
                 forceKeyline ? R.menu.uncheck_menu : R.menu.check_menu);
-        listItemView1.forceKeyline(forceKeyline);
-        listItemView2.forceKeyline(forceKeyline);
-        listItemView3.forceKeyline(forceKeyline);
+        listItemView.forceKeyline(forceKeyline);
     }
 
     private void onAttrIconClicked() {
@@ -135,16 +121,12 @@ public class MainActivity extends AppCompatActivity {
         attributeIconView.inflateMenu(icon ? R.menu.uncheck_menu : R.menu.check_menu);
         Drawable iconDrawable = icon ? ContextCompat.getDrawable(this, R.drawable.ic_call_24dp)
                 : null;
-        listItemView1.setIcon(iconDrawable);
-        listItemView2.setIcon(iconDrawable);
-        listItemView3.setIcon(iconDrawable);
+        listItemView.setIcon(iconDrawable);
     }
 
     private void onAttrCircularIconClicked() {
         circularIcon = !circularIcon;
         attributeCircularIconView.inflateMenu(circularIcon ? R.menu.uncheck_menu : R.menu.check_menu);
-        listItemView1.useCircularIcon(circularIcon);
-        listItemView2.useCircularIcon(circularIcon);
-        listItemView3.useCircularIcon(circularIcon);
+        listItemView.useCircularIcon(circularIcon);
     }
 }
